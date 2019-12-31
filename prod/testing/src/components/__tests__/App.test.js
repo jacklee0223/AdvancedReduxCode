681b06d82302cc2d379b6fcfcf3c8ebc937b7905
Jeong Min Lee <jacklee0223@gmail.com>
2019-12-31 16:19:25 -0600

    add test for commentlist

--- prod/testing/src/components/__tests__/App.test.js
+++ prod/testing/src/components/__tests__/App.test.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../App';
 import CommentBox from '../CommentBox';
+import CommentList from '../CommentList';
 
 it('shows a comment box', () => {
   const shallowComponent = shallow(<App />);
 
   expect(shallowComponent.find(CommentBox).length).toEqual(1);
+  expect(shallowComponent.find(CommentList).length).toEqual(1);
 });
