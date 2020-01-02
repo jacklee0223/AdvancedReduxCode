26a1aabb11f6d3f6856c80eaae21330c3da951e8
Jeong Min Lee <jacklee0223@gmail.com>
2020-01-01 20:29:01 -0600

    implement change event test

--- prod/testing/src/components/__tests__/CommentBox.test.js
+++ prod/testing/src/components/__tests__/CommentBox.test.js
@@ -16,3 +16,19 @@ it('has a text area and a button', () => {
   expect(fullDOMComponent.find('textarea').length).toEqual(1);
   expect(fullDOMComponent.find('button').length).toEqual(1);
 });
+
+it('has a text area that users can type in', () => {
+  const mockEventObj = {
+    target: {
+      value: 'new comment'
+    }
+  };
+  fullDOMComponent
+    .find('textarea')
+    .simulate('change', mockEventObj)
+    .update();
+
+  expect(fullDOMComponent.find('textarea').prop('value')).toEqual(
+    'new comment'
+  );
+});
