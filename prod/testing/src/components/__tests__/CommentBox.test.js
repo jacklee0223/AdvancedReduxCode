ec9fe479b5a42e097caf83de1c702db505fa67b2
Jeong Min Lee <jacklee0223@gmail.com>
2020-01-01 19:28:14 -0600

    implement full dom test for commentbox

---
+++ prod/testing/src/components/__tests__/CommentBox.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import CommentBox from 'components/CommentBox';
+
+let fullDOMComponent;
+
+beforeEach(() => {
+  fullDOMComponent = mount(<CommentBox />);
+});
+
+it('has a text area and a button', () => {
+  const fullDOMComponent = mount(<CommentBox />);
+
+  expect(fullDOMComponent.find('textarea').length).toEqual(1);
+  expect(fullDOMComponent.find('button').length).toEqual(1);
+});
