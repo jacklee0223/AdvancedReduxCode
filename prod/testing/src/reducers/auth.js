1bdcd2872571be684eec5451ffefae43d803eb6e
Jeong Min Lee <jacklee0223@gmail.com>
2020-01-04 11:57:13 -0600

    implement header

---
+++ prod/testing/src/reducers/auth.js
@@ -0,0 +1,12 @@
+import { CHANGE_AUTH } from 'actions/types';
+
+export default (state = false, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case CHANGE_AUTH:
+      return payload;
+    default:
+      return state;
+  }
+};
