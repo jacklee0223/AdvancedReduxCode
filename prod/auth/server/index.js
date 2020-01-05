475f5071ed836a799aada70d50f0d6d517f40a03
Jeong Min Lee <jacklee0223@gmail.com>
2020-01-05 16:28:14 -0600

    add middlewares and nodemon

--- prod/auth/server/index.js
+++ prod/auth/server/index.js
@@ -6,6 +6,9 @@ const morgan = require('morgan');
 const app = express();
 
 // App Setup
+// Middlewares
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
 
 // Server Setup
 const port = process.env.PORT || 3090;
