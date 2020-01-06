71d5188f698b2daceba3cd59c567cdf46abd2a3a
Jeong Min Lee <jacklee0223@gmail.com>
2020-01-06 13:19:59 -0600

    use cors on server

--- prod/auth/client/src/components/auth/Signup.jsx
+++ prod/auth/client/src/components/auth/Signup.jsx
@@ -1,18 +1,50 @@
 import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import authReducer from 'reducers/auth';
+import * as actions from 'actions';
 
 class Signup extends Component {
+  onSubmit = formProps => {
+    this.props.signup(formProps);
+  };
+
   render() {
+    // handleSubmit comes from reduxForm
+    const { handleSubmit } = this.props;
+
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.onSubmit)}>
         <fieldset>
           <label>Email</label>
+          <Field
+            name="email"
+            type="text"
+            component="input"
+            autoComplete="none"
+          />
         </fieldset>
         <fieldset>
           <label>Password</label>
+          <Field
+            name="password"
+            type="password"
+            component="input"
+            autoComplete="none"
+          />
         </fieldset>
+        <button>Sign Up</button>
       </form>
     );
   }
 }
 
-export default Signup;
+// const mapStateToProps = {
+//   auth: authReducer
+// };
+
+export default compose(
+  connect(null, actions),
+  reduxForm({ form: 'singup' })
+)(Signup);
