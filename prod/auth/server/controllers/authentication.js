93512c0f30303752ca93392a2db5d4b80353b8f1
Jeong Min Lee <jacklee0223@gmail.com>
2020-01-05 19:32:59 -0600

    send back jwt token on auth success

--- prod/auth/server/controllers/authentication.js
+++ prod/auth/server/controllers/authentication.js
@@ -1,7 +1,13 @@
+const jwt = require('jwt-simple');
 const User = require('../models/user');
+const config = require('../config');
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
 
 exports.signup = (req, res, next) => {
-  console.log(req.body);
   const { email, password } = req.body;
 
   if (!email || !password) {
@@ -29,7 +35,7 @@ exports.signup = (req, res, next) => {
       if (error) return next(error);
 
       // Respond to request indicating user was created
-      res.json({ success: true });
+      res.json({ success: true, token: tokenForUser(newUser) });
     });
   });
 };
