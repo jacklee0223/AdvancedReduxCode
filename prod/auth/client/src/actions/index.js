c57f34029d9402beab519170852e0390a17fad4c
Jeong Min Lee <jacklee0223@gmail.com>
2020-01-06 13:23:53 -0600

    implement auth user reducer

--- prod/auth/client/src/actions/index.js
+++ prod/auth/client/src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { AUTH_USER } from 'actions/types';
 
-export const signup = formProps => dispatch => {
-  axios.post('http://localhost:3090/signup', formProps);
+export const signup = formProps => async dispatch => {
+  const response = await axios.post('http://localhost:3090/signup', formProps);
+  dispatch({ type: AUTH_USER, payload: response.data.token });
 };
--- prod/auth/client/src/reducers/auth.js
+++ prod/auth/client/src/reducers/auth.js
@@ -1,10 +1,16 @@
+import { AUTH_USER } from 'actions/types';
+
 const INITIAL_STATE = {
   authenticated: '',
   errorMessage: ''
 };
 
 export default (state = INITIAL_STATE, action) => {
-  switch (action) {
+  const { type, payload } = action;
+
+  switch (type) {
+    case AUTH_USER:
+      return { ...state, authenticated: payload };
     default:
       return state;
   }
